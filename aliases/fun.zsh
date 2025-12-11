87af6f32847bd01cfd25eb8f025aad7318a37dfd
Jungwoo Shin <jungwooshin.97@gmail.com>
2025-12-11 17:43:01 +0900

    feat:weather2

--- aliases/fun.zsh
+++ aliases/fun.zsh
@@ -1,5 +1,6 @@
 alias alert='notify-send --urgency=low -i "$([ $? = 0 ] && echo terminal || echo error)" "$(history|tail -n1|sed '\''s/^\s*[0-9]+\s*//;s/[;&|]*$//'\'')"'
 alias weather='curl wttr.in'
+alias weather2='curl v2.wttr.in/seoul'
 alias shrug='echo "¯\_(ツ)_/¯" | wl-copy && echo "¯\_(ツ)_/¯"'
 alias tableflip='echo "(╯°□°）╯︵ ┻━┻" | wl-copy && echo "(╯°□°）╯︵ ┻━┻"'
 
